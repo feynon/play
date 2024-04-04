08c10bcd89c319d1a3738a9ecce93b80f94d2734
Ankesh Bharti <hey@ankeshbharti.com>
2024-04-04 18:42:24 +0530

    Add extism setup

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { usePlugin, ExtismPlugin } from '@extism/react'
+
+const pluginSource = 'https://modsurfer.dylibso.workers.dev/api/v1/module/93898457953d30d016f712ccf4336ce7e9971db5f7f3aff1edd252764f75d5d7.wasm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { plugin, loading: pluginLoading, useFunction } = usePlugin(pluginSource, { useWasi: true })
+  const [hookInput, setHookInput] = useState('')
+  const [componentInput, setComponentInput] = useState('')
 
+  const [output, setOutput] = useState('')
+  useEffect(() => {
+    plugin?.call('count_vowels', hookInput).then((output) => {
+      setOutput(new TextDecoder().decode(output))
+    })
+  }, [plugin, hookInput])
+
+  /**
+   * Here's how to call the function with the `useFunction` convenience hook
+   * const { output, loading } = useFunction('count_vowels', hookInput);
+   * // output at this point is an ArrayBuffer, so you will need to decode to text like:
+   * // const decoder = new TextDecoder()
+   * // decoder.decode(output)
+   */
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="example-hook">
+        <div className="header">Hook</div>
+        {pluginLoading ? 'Loading plugin...' : (
+          <div>
+            <input onChange={(e) => {
+              setHookInput(e.target.value)
+            }} />
+            {output}
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="example-component">
+        <div className="header">Component</div>
+        {pluginLoading ? 'Loading plugin...' : (
+          <div>
+            <input onChange={(e) => {
+              setComponentInput(e.target.value)
+            }} />
+            <ExtismPlugin source={pluginSource} input={componentInput} functionName='count_vowels' />
+            {/* it could also be used like this:
+              <ExtismPlugin source={pluginSource} input={componentInput} functionName='count_vowels'>
+                {(output) => {
+                  return new TextDecoder().decode(output)
+                }}
+              </ExtismPlugin> */}
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
--- host/src/repl.js
+++
@@ -1,9 +0,0 @@
-const createPlugin = require("@extism/extism")
-
-const plugin = await createPlugin(
-    'https://github.com/extism/plugins/releases/latest/download/count_vowels.wasm',
-    { useWasi: true }
-);
-let out = await plugin.call("count_vowels", "Hello, World!");
-console.log(out.text())
-// => '{"count":3,"total":3,"vowels":"aeiouAEIOU"}'
